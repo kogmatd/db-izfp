87c56b0f8c6c64e47ec48dcae5720051f1729dec
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2019-02-20 12:26:03 +0100

    update

--- scripts/oneclass_ubm.py
+++ scripts/oneclass_ubm.py
@@ -190,7 +190,6 @@ for cls in clsuse:
     for fea in feause:
         if cls=='hmm' and ftrns[sen[0]][0][fea].shape[-1]>40: continue
         probfn=os.path.join(dlog,'prob_'+cls+'_'+fea+'.npy')
-        print(probfn)
         if os.path.exists(probfn): continue
         kwargs=icfg.get('trnargs.%s.%s'%(cls,fea))
         if kwargs is None: kwargs={}
